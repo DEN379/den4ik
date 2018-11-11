79b693f4c2e8dd959a36645cfa7b494b1f0b173d
DEN379 <43930559+DEN379@users.noreply.github.com>
2018-11-11 12:48:45 +0200

    lab2

---
+++ Main.java
@@ -0,0 +1,15 @@
+package oop_l2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BankAccount ba=new BankAccount(200);
+		System.out.println(ba.getBalance());
+		ba.addMoney(100);
+		System.out.println(ba.getBalance());
+		ba.subMoney(250);
+		System.out.println(ba.getBalance());
+
+	}
+
+}
