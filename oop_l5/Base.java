da841a276d6121022a7a274f6bd5ca0c99a73cff
DEN379 <43930559+DEN379@users.noreply.github.com>
2018-12-02 14:12:47 +0200

    Add files via upload

---
+++ oop_l5/Base.java
@@ -0,0 +1,30 @@
+package oop_l5;
+
+public class Base {
+	
+	String name;
+	public Base() {
+		name="Base";
+	}
+	void myname() {
+		System.out.println("This is class "+name);
+	}
+}
+
+class DerA extends Base{
+	public DerA() {
+		name="DerA";
+	}
+}
+
+class DerB extends DerA{
+	public DerB() {
+		name="DerB";
+	}
+}
+
+class DerAB extends DerB{
+	public DerAB() {
+		name="DeraAB";
+	}
+}
